95b21e17e680d4d6170d4361db4339baf9ec5be4
NAMRATASINGH15 <namratasinghvns7@gmail.com>
2024-05-29 04:15:53 +0530

    Array Traversal

---
+++ src/ArrayTraversal.java
@@ -0,0 +1,22 @@
+public class ArrayTraversal {
+
+    public static void main(String[] args) {
+
+        //Decalartion and Instantiation
+        int[] arr = new int[6];
+
+        //Initialization
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        arr[5] = 60;
+
+        //Treverse Array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
